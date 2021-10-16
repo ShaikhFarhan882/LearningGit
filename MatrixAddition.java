8f39f18e55fa061283141a1a3f1eb266742bd4e8
FarhanOP <blaxerf@gmail.com>
2021-10-16 08:07:07 -0700

    Matrix program added

---
+++ MatrixAddition.java
@@ -0,0 +1,113 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+import java.util.Scanner;
+ 
+class AddMatrix
+{
+public static void main(String args[])
+{
+int row, col,i,j;
+Scanner in = new Scanner(System.in);
+ 
+System.out.println("Enter the number of rows");
+row = in.nextInt();
+ 
+System.out.println("Enter the number columns");
+col = in.nextInt();
+ 
+int mat1[][] = new int[row][col];
+int mat2[][] = new int[row][col];
+int res[][] = new int[row][col];
+ 
+System.out.println("Enter the elements of matrix1");
+ 
+for ( i= 0 ; i < row ; i++ )
+{ 
+ 
+for ( j= 0 ; j < col ;j++ )
+mat1[i][j] = in.nextInt();
+ 
+System.out.println();
+}
+System.out.println("Enter the elements of matrix2");
+ 
+for ( i= 0 ; i < row ; i++ )
+{
+ 
+for ( j= 0 ; j < col ;j++ )
+mat2[i][j] = in.nextInt();
+ 
+System.out.println();
+}
+ 
+for ( i= 0 ; i < row ; i++ )
+for ( j= 0 ; j < col ;j++ )
+res[i][j] = mat1[i][j] + mat2[i][j] ; 
+ 
+System.out.println("Sum of matrices:-");
+ 
+for ( i= 0 ; i < row ; i++ )
+{ 
+for ( j= 0 ; j < col ;j++ )
+System.out.print(res[i][j]+"\t");
+ 
+System.out.println();
+}
+ 
+}
+}
